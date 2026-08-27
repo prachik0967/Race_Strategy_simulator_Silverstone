383fc818d2b5a2e4fa2c32f74a1088b6f19e43ce
prachik0967 <prachipkoundinya@outlook.com>
2026-08-27 15:01:23 +0100

    Add circuit parameters for Silverstone Grand Prix - CODE
    Added parameters and initial display for Silverstone circuit information.

---
+++ 01_problem_definition/circuit_parameters.py
@@ -0,0 +1,14 @@
+# Parameters need to be declared
+
+circuit_name = "Silverstone Grand Prix Circuit"
+circuit_length = 5.891 # this length is in km
+race_laps = 52
+race_distance = 306.198 # length in km
+
+# Inintally display some of the critical circuit information
+
+print("Silverstone race information")
+print(f"Circuit: {circuit_name}")
+print(f"Circuit Length: {circuit_length} km")
+print(f"Race Laps: {race_laps}")
+print(f"Race Distance: {race_distance} km")
